605fff24da65124fe29346cbf55fc95b760253ac
Victor Raony <victor_raony12@hotmail.com>
2021-03-17 13:25:38 -0400

    Incluido DATA para manipução das aulas

---
+++ data.js
@@ -0,0 +1,39 @@
+module.exports = [
+    {
+        id: "s3eqhq0dRnE",
+        title: "Power BI, Menu customizado usando JSON | Karine Lago",
+        duration: "57 min",
+        price:"Free"
+    },
+    {
+        id: "0RFZihkovU4",
+        title: "Power BI, Dashboard de acões do Google | Karine Lago",
+        duration: "57 min",
+        price:"Free"
+    },
+    {
+        id: "w3C3Tvucu0c",
+        title: "Power BI, Quanto cobrar para realizar um projeto em Poer BI? | Karine Lago",
+        duration: "57 min",
+        price:"Free"
+    },
+    {
+        id: "pVsj5iZfxpY",
+        title: "Power BI, Versão Premium por Usuario está disponivel, confira o video | Karine Lago",
+        duration: "57 min",
+        price:"Free"
+    },
+    {
+        id: "0aPXOnrjrvI",
+        title: "Power BI, 11 Formas de compartilhar relatorios entre usuarios | Karine Lago",
+        duration: "57 min",
+        price:"Free"
+    },
+    {
+        id: "UJKGM-HqnNU",
+        title: "Power BI, Visiuais, indicadores de performance | Karine Lago",
+        duration: "57 min",
+        price:"Free"
+    },
+
+]
