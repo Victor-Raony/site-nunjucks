e176345dccd62b17917357a8450dc04a222a9f30
Victor Raony <victor_raony12@hotmail.com>
2021-03-17 13:27:11 -0400

    Configuração do server para remoção do HTML das views

--- server.js
+++ server.js
@@ -2,21 +2,36 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    autoescape: false
 })
 
 server.get("/", function(req, res){
-    return res.render("about")
+    const about = {
+        avatar_url: "https://avatars.githubusercontent.com/u/67339446?s=460&u=32ba747f1f1bbd7a7a0fd761c1c2cb1453c11e07&v=4",
+        name: "Victor Raony",
+        role: "Analista de Sistemas",
+        description: 'Estudando programação com a <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>, focado em HTML, CSS, JAVASCRIPT, REACT, REACT NATIVE, TYPESCRIPT, NODE.JS',
+        links: [
+            {name: "Github", url: "https://github.com/Victor-Raony" },
+            {name: "Twitter", url: "/" },
+            {name: "Linkedin", url: "https://www.linkedin.com/in/victor-raony-10a385132" },            
+        ]
+    }
+
+
+    return res.render("about", { about})
 })
 
 server.get("/portfolio", function(req, res){
-    return res.render("portfolio")
+    return res.render("portfolio", {items: videos})
 })
 
 server.listen(5000, function(){
